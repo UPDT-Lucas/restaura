ccc8af37ad491e181f9a2d112e8ffbb0cf4aeda7
Gabox13 <gabops0511@gmail.com>
2025-04-08 20:42:47 -0600

    trabajo medio terminado de recuperar

--- backEnd/controllers/clienteController.js
+++ backEnd/controllers/clienteController.js
@@ -164,6 +164,161 @@ clienteCtr.saveCliente = async (req,res)=> {
     }
 }
 
+clienteCtr.getClienteAll = async (req,res)=> {
+    const p_id = req.params.id; 
+    console.log("Parametros recibidos:", req.params.id ,p_id); 
+    try {
+        const sequelize = dbConnection.getInstance().Sequelize; 
+        let finalRes = {};
+
+        const [clientService, metadata] = await sequelize.query(
+            'SELECT * FROM fn_get_cliente_servicio(:p_id)',
+            {
+                replacements: { p_id},
+                type: sequelize.QueryTypes.SELECT, 
+            }
+        );
+
+        finalRes.personal = clientService;
+        const [clientService1, metadata1] = await sequelize.query(
+            'SELECT * FROM fn_get_info3meses(:p_id)',
+            {
+                replacements: { p_id},
+                type: sequelize.QueryTypes.SELECT, 
+            }
+        );
+        
+        finalRes.info3meses_id = clientService1;
+        
+        const [clientService2, metadata2] = await sequelize.query(
+            'SELECT * FROM fn_get_contacto(:p_id)',
+            {
+                replacements: { p_id},
+                type: sequelize.QueryTypes.SELECT, 
+            }
+        );
+        if(clientService2 !== undefined && clientService2 !== null){
+            finalRes.contacto= clientService2;
+        }else{
+            finalRes.contacto = null;
+        }
+        
+
+        const [clientService3, metadata3] = await sequelize.query(
+            'SELECT * FROM fn_get_informacion_inamu(:p_id)',
+            {
+                replacements: { p_id},
+                type: sequelize.QueryTypes.SELECT, 
+            }
+        );
+
+
+
+        if(clientService3 !== undefined && clientService3 !== null){
+            finalRes.inamu= clientService3;
+
+            const clientService4 = await sequelize.query(
+                'SELECT * FROM fn_get_tipos_ayuda(:p_id)',
+                {
+                  replacements: { p_id },
+                  type: sequelize.QueryTypes.SELECT,
+                }
+              );
+            console.log("Resultados de clientService4:", clientService4);
+            finalRes.tipos_ayuda = clientService4;
+
+            const [clientService5, metadata5] = await sequelize.query(
+                'SELECT * FROM fn_get_tipo_violencia(:p_id)',
+                {
+                    replacements: { p_id},
+                    type: sequelize.QueryTypes.SELECT, 
+                }
+            );
+            finalRes.tipos_violencia = clientService5;
+
+            const [clientService6, metadata6] = await sequelize.query(
+                'SELECT * FROM fn_get_instituciones_violencia(:p_id)',
+                {
+                    replacements: { p_id},
+                    type: sequelize.QueryTypes.SELECT, 
+                }
+            );
+            finalRes.instituciones_violencia = clientService6;
+
+            
+
+        }else{
+            finalRes.inamu = null;
+            finalRes.tipos_ayuda = [];
+            finalRes.tipos_violencia = [];
+            finalRes.instituciones_violencia = [];
+        }
+
+        
+
+
+        
+
+        console.log("Resultados del procedimiento:", finalRes);
+
+    res.status(200).json(finalRes); 
+    } catch (error) {
+        console.error('Error al obtener el cliente y servicio:', error);
+        res.status(500).json({ message: 'Error al obtener el cliente y servicio', error: error.message });
+    }
+}
+
+
+clienteCtr.getCliente = async (req,res)=> {
+    const { p_id, p_nombre } = req.params; 
+    console.log("Parametros recibidos:", p_id, p_nombre); 
+
+    try {
+        const sequelize = dbConnection.getInstance().Sequelize; 
+
+
+        const [clientService, metadata] = await sequelize.query(
+            'SELECT * FROM buscar_cliente(:p_id, :p_nombre)',
+            {
+                replacements: { p_id, p_nombre },
+                type: sequelize.QueryTypes.SELECT, 
+            }
+        );
+
+        console.log("Resultados del procedimiento:", clientService);
+
+    res.status(200).json(clientService); 
+    } catch (error) {
+        console.error('Error al obtener el cliente y servicio:', error);
+        res.status(500).json({ message: 'Error al obtener el cliente y servicio', error: error.message });
+    }
+}
+
+
+
+
+clienteCtr.deleteCliente = async (req,res)=> {
+    try{
+        const db = dbConnection.getInstance();
+        const sequelize = db.Sequelize;
+        const data = req.body;
+
+
+        const result = await sequelize.query('CALL deletecliente_servicio(:p_id, :p_inamu_id)', { 
+            replacements: { p_id: data.cliente_servicio_id, p_inamu_id: data.informacion_inamu_id },
+            type: sequelize.QueryTypes.RAW
+        });
+
+        res.status(200).json({ message: "Usuario correctamente eliminado",
+            cliente: result,status: 200});
+
+    }catch(error){
+        console.error("Error al eliminar cliente:", error);
+        res.status(500).json({ message: "Error al eliminar cliente", error });
+    }
+}
+
+
 export default clienteCtr;
 
 
--- backEnd/routes/cliente.routes.js
+++ backEnd/routes/cliente.routes.js
@@ -3,6 +3,9 @@ import clienteCtr from '../controllers/clienteController.js';
 
 var api = express.Router();
 
+api.get('/clients/:p_id/:p_nombre', clienteCtr.getCliente);
+api.get('/getClienteAll/:id', clienteCtr.getClienteAll);
 api.post('/saveCliente', clienteCtr.saveCliente);
+api.delete('/deleteCliente', clienteCtr.deleteCliente);
 
 export default api;
