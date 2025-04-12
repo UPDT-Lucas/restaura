3b75578856efd6e98af2e06b785c6131f1a889a4
JonathanMFV <80858858+JonathanMFV@users.noreply.github.com>
2025-04-12 17:24:58 -0600

    Cambios header y dropdown

--- frontEnd/src/app/asistente/pages/log/log.component.ts
+++ frontEnd/src/app/asistente/pages/log/log.component.ts
@@ -3,28 +3,28 @@ import { TableComponent } from '../../../shared/components/table/table.component
 import { SearchBoxComponent } from '../../../shared/components/search-box/search-box.component';
 import { ClientService } from '../../../services/client.service';
 import { ClienteServicio } from '../../../interfaces/clienteServicio.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-log',
-  imports: [
-    TableComponent,
-    SearchBoxComponent
-  ],
-  templateUrl: './log.component.html',
-  styleUrl: './log.component.css'
+    selector: 'app-log',
+    imports: [TableComponent, SearchBoxComponent, CommonModule],
+    templateUrl: './log.component.html',
+    styleUrl: './log.component.css',
 })
 export class LogComponent {
-  constructor(private clientService: ClientService){}
+    cargando: boolean = true;
+    constructor(private clientService: ClientService) {}
 
-  clients: ClienteServicio[] = [];
+    clients: ClienteServicio[] = [];
 
-  ngOnInit() {
-    this.searchClientById("");
-  }
+    ngOnInit() {
+        this.searchClientById('');
+    }
 
-  searchClientById(id: string): void {
-    this.clientService.getClients(id, "10", "0").subscribe((data: ClienteServicio []) => {
-      this.clients = data;
-    }); 
-  }
+    searchClientById(id: string): void {
+        this.clientService.getClients(id, '10', '0').subscribe((data: ClienteServicio[]) => {
+            this.clients = data;
+            this.cargando = false;
+        });
+    }
 }
