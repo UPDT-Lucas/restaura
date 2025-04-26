4c931d94b27a2f3341e68025f31b7f55c9421a3b
Gabox13 <gabops0511@gmail.com>
2025-04-25 22:08:43 -0600

    cambio de ruta

--- backEnd/routes/admin.route.js
+++ backEnd/routes/admin.route.js
@@ -3,6 +3,6 @@ import adminCtr from '../controllers/adminController.js';
 
 var api = express.Router();
 
-api.post('/saveAdmin', adminCtr.saveAsistente);
+api.post('/saveAsistente', adminCtr.saveAsistente);
 
 export default api;
