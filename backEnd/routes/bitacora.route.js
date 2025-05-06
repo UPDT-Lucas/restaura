ecfabf1f391cae6fa8e809709551b7c62f64b198
Gabox13 <gabops0511@gmail.com>
2025-05-05 22:21:07 -0600

    last room modificado y delete para varios

--- backEnd/routes/bitacora.route.js
+++ backEnd/routes/bitacora.route.js
@@ -5,8 +5,9 @@ import bitacoraCtr from '../controllers/bitacoraController.js';
 var api = express.Router();
 
 api.get('/bitacora/:bitacora_date', bitacoraCtr.getBitacora);
-api.get('/getLastRoom/:id', bitacoraCtr.getLastRoom);
+api.get('/getLastRoom/:id/:idBitacora', bitacoraCtr.getLastRoom);
 api.post('/clienteSaveBitacora',bitacoraCtr.clienteSaveBitacora);
 api.delete('/clienteDeleteBitacora',bitacoraCtr.clienteDeleteBitacora);
+api.post('/createBitacora/:bitacora_date',bitacoraCtr.createBitacora)
 
 export default api;
