ce2951ca5b2f385ccfaaaea0851532f723469186
Gabox13 <88801469+Gabox13@users.noreply.github.com>
2025-05-08 14:03:25 -0600

    Update admin.route.js

--- backEnd/routes/admin.route.js
+++ backEnd/routes/admin.route.js
@@ -4,5 +4,6 @@ import adminCtr from '../controllers/adminController.js';
 var api = express.Router();
 
 api.post('/saveAsistente', adminCtr.saveAsistente);
+api.get('/auth',adminCtr.auth);
 
 export default api;
