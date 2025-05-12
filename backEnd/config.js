d7d3637138fcc428ff87e60157246e0aef6c0981
Gabox13 <gabops0511@gmail.com>
2025-05-12 16:31:58 -0600

    cambios de variable

--- backEnd/config.js
+++ backEnd/config.js
@@ -4,10 +4,10 @@ dotenv.config();
 const config = {
     HOST: process.env.HOST || "localhost",
     PORT: process.env.PORT || 3100,
-    DB: process.env.DB || "localhost",
-    DB_NAME: process.env.DB_NAME || "centroDormitorio",
-    DB_PASS: process.env.DB_PASS ||  "Restaura1314",
-    DB_USER: process.env.DB_USER || "postgres",
+    DB: process.env.DB || "aws-0-us-east-1.pooler.supabase.com",
+    DB_NAME: process.env.DB_NAME || "postgres",
+    DB_PASS: process.env.DB_PASS ||  "Gabonator1314",
+    DB_USER: process.env.DB_USER || "postgres.vwqewolwvryzamagkxef",
     DB_PORT: process.env.DB_PORT || 5432
 }
 export default config;
