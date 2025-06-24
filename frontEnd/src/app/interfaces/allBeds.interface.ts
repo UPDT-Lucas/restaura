2678ca498205d103c72d9de02e24c2ff494a9b7b
Gabox13 <gabops0511@gmail.com>
2025-06-23 18:19:13 -0600

    cambios extra

--- frontEnd/src/app/interfaces/allBeds.interface.ts
+++ frontEnd/src/app/interfaces/allBeds.interface.ts
@@ -8,6 +8,8 @@ export interface Bed {
     nombre:       string;
     active:       boolean;
     tipo_cama_id: number;
+    ocupado:        boolean;
+    cliente_servicio_id: number | null;
     cuarto_id:    number;
     tipo_cama:    TipoCama;
 }
